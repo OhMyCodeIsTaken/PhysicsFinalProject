c31d15852fcdca77046f726391818543c3596256
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2023-05-09 14:13:25 +0300

    IDK

--- Assets/Scripts/ResetPlayBallPosition.cs
+++ Assets/Scripts/ResetPlayBallPosition.cs
@@ -18,7 +18,7 @@ public class ResetPlayBallPosition : MonoBehaviour
         foreach (var child in _children)
         {
             // This exists because of a bug where a ball gets stuck between two balls that are used as bounds
-            child.OnCollisionWith += _playBallsRespawnPoints.RespawnBall;
+            //child.OnCollisionWith += _playBallsRespawnPoints.RespawnBall;
         }
     }
 }
